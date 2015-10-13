3357bf3da187cd2762da2d7357f72ed453dce592
Dumitru Cantea <canteadumitru@gmail.com>
2015-10-13 17:13:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  AccessService: require('./lib/db/access.js'),
+  CacheAccessService: require('./lib/db/cache_access.js'),
+  ControlService: require('./lib/db/control.js'),
+  SearchService: require('./lib/search.js'),
+  util: require('./lib/util.js')
+};
